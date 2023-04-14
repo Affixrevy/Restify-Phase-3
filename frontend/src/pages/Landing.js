93562332a7f8f23ccd2d84a2210353ebf78ca7c5
Arthur Gao <arthurerconggao@gmail.com>
2023-04-14 14:20:28 -0400

    [FIX] new infinite scroll system

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -4,77 +4,67 @@ import SearchBar from "../components/SearchBar";
 import FilterBar from "../components/FilterBar";
 import PropertyCard from "../components/PropertyCard";
 import propertyCard from "../components/PropertyCard";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-const Landing = () => {
-    const [properties, setProperties] = useState([]);
-    const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(true);
-    const [hasMore, setHasMore] = useState(false);
-    const dataFetchedRef = useRef(false)
-
-    useEffect(() => {
-        if (!dataFetchedRef.current) {
-            setLoading(true);
-            fetch(`http://localhost:8000/properties/view/?page=${page}`)
-                .then(response => response.json())
-                .then(data => {
-                    setProperties(prevProperties => [...prevProperties, ...data.results]);
-                    console.log(data.results)
-                    dataFetchedRef.current = true;
-                    setHasMore(data.next !== null);
-                    setLoading(false);
-                })
-                .catch(error => {
-                    console.error(error);
-                    setLoading(false);
-                });
-        }
-    }, [page]);
-
-
-    function handleScroll() {
-        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-            if (!loading && hasMore) {
-                dataFetchedRef.current = false;
-                setPage(prevPage => prevPage + 1);
-            }
+class Landing extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            hasMore: true
         }
     }
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, [handleScroll]);
+    componentDidMount() {
+        this.fetchData(1)
+    }
 
+    fetchData = (page) => {
+        fetch(`http://localhost:8000/properties/view/?page=${page}`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({items: [...this.state.items, ...data.results], hasMore: data.next !== null})
+                console.log(data)
+            })
+            .catch(error => {
+                console.error(error);
+            });
 
-    return (
-        <body className="h-screen bg-BACKGROUND_COLOR_1">
-        <NavBar></NavBar>
-        <SearchBar></SearchBar>
-        <main className="flex justify-center lg:mt-16 py-5 mx-auto w-full">
-            <div className="w-11/12 md:w-2/3">
-                <div className="py-4">
-                    <FilterBar></FilterBar>
+    }
+
+    render() {
+        return (
+            <body className="h-screen bg-BACKGROUND_COLOR_1">
+            <NavBar></NavBar>
+            <SearchBar></SearchBar>
+            <main className="flex justify-center lg:mt-16 py-5 mx-auto w-full">
+                <div className="w-11/12 md:w-2/3">
+                    <div className="py-4">
+                        <FilterBar></FilterBar>
+                    </div>
+                    <InfiniteScroll
+                        next={() => this.fetchData(this.state.items.length / 8 + 1)}
+                        hasMore={this.state.hasMore}
+                        loader={<h4>Loading...</h4>}
+                        dataLength={this.state.items.length}
+                        scrollThreshold={1}
+                        scrollableTarget="window"
+                        // children={}
+                        endMessage={<p className="mt-2 p-2 text-FONT_COLOR_2 text-sm text-center justify-center">No more
+                            properties
+                            to
+                            load</p>}>
+                        <div className="grid lg:grid-cols-2 gap-4 w-full">
+                            {this.state.items.map((item, index) => {
+                                return <PropertyCard property={item}></PropertyCard>
+                            })}
+                        </div>
+                    </InfiniteScroll>
                 </div>
-                <div className="grid lg:grid-cols-2 gap-4 w-full">
-
-                    {properties.length > 0 ? (
-                        properties.map((item, index) => {
-                            return <PropertyCard property={item}></PropertyCard>
-                        })
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-
-                </div>
-                {loading && <p className="text-center justify-center ">Loading more properties...</p>}
-                {!loading && !hasMore &&
-                    <p className="mt-2 p-2 text-FONT_COLOR_2 text-sm text-center justify-center">No more properties to
-                        load</p>}
-            </div>
-        </main>
-        </body>
-    );
-};
+            </main>
+            </body>
+        );
+    }
+}
 
 export default Landing;
